32e34b08d657198bca9bd5b8e7082e00b480edd2
Santosh Achari <santosh@acharis.com>
2016-09-07 18:42:27 +0200

    Added Laracademy (Model) Generator (https://github.com/laracademy/generators)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //Only Development plugins
         if ($this->app->environment() == 'local') {
+            // Laracasts Generators
             $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
+
+            // Laracademy Model Generators
+            $this->app->register('\Laracademy\Generators\GeneratorsServiceProvider');
         }
     }
 }
